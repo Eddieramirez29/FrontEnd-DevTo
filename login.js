c9caeb6dea107df568ccb59859c6347377cf187c
Eduardo <tkd13000@gmail.com>
2023-09-16 12:54:17 -0600

    Se agrega el .js para poder ir de la página man a la página de logueo

---
+++ login.js
@@ -0,0 +1,7 @@
+const buttonLogin = document.querySelector(".btn__link-LogIn");
+
+buttonLogin.addEventListener("click", function()
+{
+    //Ir a la página de logueo.
+    window.location.href = "http://127.0.0.1:5500/login.html";
+});
