3003147c5e40639dd88dae82668d651b94e3d7e6
Eduardo <tkd13000@gmail.com>
2023-09-16 18:46:31 -0600

    Mensaje de alerta después de la suscripción

--- sendForm.js
+++ sendForm.js
@@ -1,14 +1,14 @@
 const buttonRegister = document.getElementById('Register-form');
 
 buttonRegister.addEventListener("submit", async (e) => {
-  e.preventDefault();
+  // e.preventDefault();
   
 
   try {
-    console.log(e.target.name.value);
-  console.log(e.target.lastName.value);
-  console.log(e.target.email.value);
-  console.log(e.target.password.value);
+  //   console.log(e.target.name.value);
+  // console.log(e.target.lastName.value);
+  // console.log(e.target.email.value);
+  // console.log(e.target.password.value);
     const respuesta =  await fetch("http://localhost:27027/signup", {
       method: "POST",
       headers: {
@@ -20,10 +20,13 @@ buttonRegister.addEventListener("submit", async (e) => {
         email: e.target.email.value,
         password: e.target.password.value
       })
-    });
-
+      
+    } );
+    setTime(alert("Suscripción exitosa!"), 1000);
    }
  
+  
+
    catch (error)
   {
     console.error("Error al realizar la solicitud:", error);
