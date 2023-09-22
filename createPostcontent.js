05b655a35edf6bcc98c22ab746035016042f1346
Eduardo <tkd13000@gmail.com>
2023-09-22 17:54:24 -0600

    Se agrega el código para generar estilos de los posts

--- createPostcontent.js
+++ createPostcontent.js
@@ -1,31 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const nombreUsuario = localStorage.getItem('usuarioPost');
-    const titulo = localStorage.getItem('tituloPost');
-    const contenido = localStorage.getItem('contenidoPost');
-  
-    // Obtén una referencia al elemento con el id "comments"
-    const divContenedorNombreUsuario = document.getElementById("comments");
-    const divContenedorTitulo = document.getElementById("comments");
-    const divContenedorContenido= document.getElementById("comments");
-  
-    // Verifica si nombreUsuario es válido
-    if (nombreUsuario) {
-      // Crea un nuevo elemento <p>
-      const parrafoNombreUsuario = document.createElement("p");
-      const parrafoNombreTitulo = document.createElement("p");
-      const parrafoNombreContenido = document.createElement("p");
-  
-      // Agrega contenido al párrafo utilizando la variable nombreUsuario
-      parrafoNombreUsuario.textContent = nombreUsuario;
-      parrafoNombreTitulo.textContent = titulo;
-      parrafoNombreContenido.textContent = contenido;
-  
-      // Agrega el párrafo al div
-      divContenedorNombreUsuario.appendChild(parrafoNombreUsuario);
-      divContenedorTitulo.appendChild(parrafoNombreTitulo);
-      divContenedorContenido.appendChild(parrafoNombreContenido);
-    } else {
-      console.error("El nombre de usuario no está definido en localStorage.");
-    }
-  });
-  
+  const nombreUsuario = localStorage.getItem('usuarioPost');
+  const titulo = localStorage.getItem('tituloPost');
+  const contenido = localStorage.getItem('contenidoPost');
+
+  // Obtén una referencia al elemento con el id "comments"
+  const divContenedor = document.getElementById("comments");
+
+  // Verifica si nombreUsuario es válido
+  if (nombreUsuario) {
+    // Crea un nuevo elemento <div> con la clase "card"
+    const cardDiv = document.createElement("div");
+    cardDiv.className = "card";
+
+    // Crea un nuevo elemento <div> con la clase "card-body"
+    const cardBodyDiv = document.createElement("div");
+    cardBodyDiv.className = "card-body";
+
+    // Crea elementos <h5>, <h6> y <p> con las clases correspondientes
+    const cardTitle = document.createElement("h5");
+    cardTitle.className = "card-title";
+    cardTitle.textContent = titulo;
+
+    const cardSubtitle = document.createElement("h6");
+    cardSubtitle.className = "card-subtitle mb-2 text-muted";
+    cardSubtitle.textContent = "Usuario: " + nombreUsuario;
+
+    const cardText = document.createElement("p");
+    cardText.className = "card-text";
+    cardText.textContent = "Contenido: " + contenido;
+
+    // Agrega los elementos al árbol DOM en el orden correcto
+    cardBodyDiv.appendChild(cardTitle);
+    cardBodyDiv.appendChild(cardSubtitle);
+    cardBodyDiv.appendChild(cardText);
+    cardDiv.appendChild(cardBodyDiv);
+    divContenedor.appendChild(cardDiv);
+  } else {
+    console.error("El nombre de usuario no está definido en localStorage.");
+  }
+});
